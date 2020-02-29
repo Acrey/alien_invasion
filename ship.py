eaf96688e7ce119a93877ee6b768ea9d42002349
Acrey <wonruner@gmail.com>
2020-03-01 01:50:07 +0300

    Add ship movements left-right

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship():
 		"""Инициализирует корабль и задает начальную позицию."""
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
+		self.settings = ai_game.settings
 
 		# Загружает изображение корабля и получает прямоугольник
 		self.image = pygame.image.load('images/ship.bmp')
@@ -13,6 +14,22 @@ class Ship():
 		# Кадлый новый корабль появляется у нижнего края экрана
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		# Флаги перемещения корабля
+		self.moving_right = False
+		self.moving_left = False
+
+		# Позиция
+		self.x = float(self.rect.x)
+
 	def blitme(self):
 		"""Рисует корабль в текущей позиции"""
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		"""Обновляет позицию корабля"""
+		if self.moving_left and self.rect.left > self.screen_rect.left:
+			self.x -= self.settings.ship_speed
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+
+		self.rect.x = self.x
