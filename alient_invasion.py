1458e8dabdc32bcf29c3b6604d0ded8bbc069d4a
Acrey <wonruner@gmail.com>
2020-02-29 04:17:55 +0300

    Init commit

---
+++ alient_invasion.py
@@ -0,0 +1,30 @@
+import sys
+
+import pygame
+
+class AlienInvasion():
+	"""Класс для управления ресурсами и поведением игры."""
+
+	def __init__(self):
+		"""Инициируем игру и создаем игровые ресурсы."""
+		pygame.init()
+
+		self.screen = pygame.display.set_mode((1200, 700))
+		pygame.display.set_caption("Alien Invasion")
+
+	def run_game(self):
+		"""Запуск основного цикла игры."""
+		while True:
+			#Отслеживание событий клавиатуры и мыши.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			#Отобраэение последнего прорисованного экрана.
+			pygame.display.flip()
+
+
+if __name__ == '__main__':
+	#Создание экземляра и запуск игры
+	ai = AlienInvasion()
+	ai.run_game()
