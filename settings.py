e314281a79639474fe0cb6f4e8014029fe2e9b16
Acrey <wonruner@gmail.com>
2020-02-29 04:38:36 +0300

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+	"""Класс для хранения всех настроек игры Alien Invasion."""
+
+	def __init__(self):
+		"""Инициализация настроек игры."""
+		#Параметры экрана
+		self.screen_width = 1200
+		self.screen_height = 700
+		self.bg_color = (230, 230, 230)
